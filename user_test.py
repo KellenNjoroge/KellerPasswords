ed89191a099af8ba6420b1b78dd62793e69a1126
KellenNjoroge <muthonkel@gmail.com>
2018-09-27 16:23:31 +0300

    created second class credentials and test cases for istantiating initializing and saving also for checking user

--- user_test.py
+++ user_test.py
@@ -30,3 +30,45 @@ class TestUser(unittest.TestCase):
 		"""
         self.new_user.save_user()
         self.assertEqual(len(User.users_list), 1)
+
+
+class TestCredentials(unittest.TestCase):
+    """
+	Test class that defines test cases for the credentials class behaviours.
+	Args:
+		unittest.TestCase: helps in creating test cases
+	"""
+    def setUp(self):
+        """
+		Function to create an account's credentials before each test
+		"""
+        self.new_credential = Credential('Kellen', 'Facebook', 'Kellen Njoroge', 'boo90')
+
+    def test__init__(self):
+        '''
+		Test to if check the initialization/creation of credential instances is properly done
+		'''
+        self.assertEqual(self.new_credential.user_name, 'Kellen')
+        self.assertEqual(self.new_credential.site_name, 'Facebook')
+        self.assertEqual(self.new_credential.account_name, 'Kellen Njoroge')
+        self.assertEqual(self.new_credential.password, 'boo90')
+    @property
+    def test_check_user(self):
+        """
+		Function to test whether the login in function check_user works as expected
+		"""
+        self.new_user = User("Mercy", "Kubania",  "kubz97")
+        self.new_user.save_user()
+        user2 = User("Mercy", "Kubania", "kubz97")
+        user2.save_user()
+
+        for user in User.users_list:
+            if user.first_name == user2.first_name and user.password == user2.password:
+                current_user = user.first_name
+        return current_user
+
+        self.assertEqual(current_user, Credential.check_user(user2.password, user2.first_name))
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
